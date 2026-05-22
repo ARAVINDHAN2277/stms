7973537e1d1461487d6713d7cd872891c34861e2
Aravindhan <aravindhan2277@gmail.com>
2026-05-22 21:33:22 +0530

    cleaned up tournament routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.JWT_SECRET)
-if (!process.env.MONGO_URI || !process.env.JWT_SECRET) {
-  console.log(process.env.JWT_SECRET)
+
+if (!process.env.DATABASE_URL || !process.env.JWT_SECRET) {
   throw new Error("Missing environment variables! Check .env file.");
 }
-import mongoose from "mongoose";
+
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import tournamentRoutes from "./routes/tournament.routes.js";
-
 
 const app = express();
 
@@ -25,8 +23,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/tournaments", tournamentRoutes);
 
 const PORT = process.env.PORT || 5000;
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => app.listen(PORT, () => console.log(`Server running on ${PORT}`)))
-  .catch((err) => console.log(err));
- 
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
